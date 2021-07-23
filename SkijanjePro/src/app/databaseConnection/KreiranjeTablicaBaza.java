08abeac34eec9ef4994c5db89e62b653aa07c758
Borko <Borko@DESKTOP-TJLDF88>
2021-07-23 10:28:23 +0200

    Kreiranje tablice klubovi bodovi

--- SkijanjePro/src/app/databaseConnection/KreiranjeTablicaBaza.java
+++ SkijanjePro/src/app/databaseConnection/KreiranjeTablicaBaza.java
@@ -77,6 +77,47 @@ public class KreiranjeTablicaBaza {
 		     }
 	
 
+	// kreiranje u bazi data tablice  Natjecatelji
+	private static boolean kreirajTablicuKluboviBodovi() throws SQLException {
+
+		try {
+
+			System.out.println("Connected");
+
+			veza = SpajanjeNaBazu.DohvatiVezu();
+
+			System.out.println("Opened database successfully");
+
+			izraz = veza.createStatement();
+
+			String sql = "CREATE TABLE IF NOT EXISTS `Natjecatelji` (\r\n"
+					+ "  `id` int(10) NOT NULL AUTO_INCREMENT,\r\n" + "  `MJESTO` varchar(200) NOT NULL,\r\n" 
+					+ "  `BROJ` varchar(200) NOT NULL,\r\n"
+					+ "  `KLUB` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,\r\n"
+					+ "  `KATEGORIJA` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,\r\n"
+					+ "  `KOD` varchar(100) NOT NULL,\r\n"
+					+ "  `PREZIME` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,\r\n"
+					+ "  `IME` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,\r\n"
+					+ "  `BODOVI` int(10),\r\n" 
+					+ "  `VRIJEME` varchar(100),\r\n" 
+					+ "  `zaostatak` varchar(100),\r\n" + "  PRIMARY KEY (`id`)\r\n"
+					+ ") ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ; ";
+
+			izraz.executeUpdate(sql);
+			
+		
+
+		} catch (Exception e4) {
+			System.err.println(e4.getClass().getName() + ": " + e4.getMessage());
+			System.exit(0);
+
+		} finally {
+
+		}
+
+		return false;
+
+	}
 	
 
 }
